dfe5c013277cbf831f51c690a82a69552eae090b
Dannisse Arenas <dannisse.a@gmail.com>
2023-03-07 22:13:18 -0500

    Create InputandOutput.cpp
    added input and output prompt

---
+++ InputandOutput.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+//GOAL: practice reading input from stdin and printing output to stdout
+//PROMPT: Read 3 numbers from stdin and print their sum to stdout
+
+
+int main() {
+    //Step 1: delcare variables 
+    int a,b,c;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cin >> a>> b>> c; //read input from string s (input is "High 5")
+
+    if (a,b,c>= 1 && a,b,c<= 1000) 
+    {
+        cout << a+b+c<< endl; //prints contents of string s, a single space, then the integer n
+    }
+    return 0;
+}
